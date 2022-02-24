f510025411acafe5d26b084305bba7bca9672411
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2022-02-24 22:26:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./components/navbar.css";
+import "./App.css";
+import Chat from "./components/chat";
+// import Footer from "./components/footer";
+import Home from "./components/Home";
+import Navbar from "./components/navbar";
+import { auth } from "./firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import logo from "./22.png";
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="front">
+        <img id="introimg" src={logo} alt="sC" />
+
+        <h2>Chathook </h2>
+        <p>Trab your conversation...</p>
+      </div>
+      <Navbar />
+      {user ? <Chat /> : <Home />}
     </div>
   );
 }
---
+++ src/main2.js
@@ -0,0 +1,97 @@
+import * as THREE from "https://unpkg.com/three@0.120.1/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.120.1/examples/jsm/controls/OrbitControls.js";
+// import { searchForWorkspaceRoot } from "vite";
+import logo from "./earth.jpg";
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById("main"),
+});
+// document.body.appendChild(renderer.domElement);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+const crystalGeometry = new THREE.SphereGeometry(12, 12, 12);
+const texture = new THREE.TextureLoader().load(logo);
+// const shinyMaterial = new THREE.MeshStandardMaterial({
+//   color: 0x4086f4,
+//   metalness: 0.1,
+//   roughness: 0.2,
+//   shininess: 0.9,
+//   // transparent: true,
+//   // opacity: 0.7,
+// });
+const shinyMaterial = new THREE.MeshStandardMaterial({
+  map: texture,
+});
+const cube = new THREE.Mesh(crystalGeometry, shinyMaterial);
+const cube2 = cube.clone();
+
+cube2.position.set(20, -50, 10);
+
+cube.position.set(30, 5, -60);
+scene.add(cube, cube2);
+
+const light = new THREE.PointLight(0xffffff);
+light.position.set(15, 15, 15);
+scene.add(light);
+
+const ambloght = new THREE.AmbientLight(0xffffff);
+scene.add(ambloght);
+
+const helper3 = new THREE.PointLightHelper(light);
+const gridhelp = new THREE.GridHelper(200, 50);
+// scene.add( gridhelp  );
+
+const control = new OrbitControls(camera, renderer.domElement);
+
+function addstar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  // const texture = new THREE.TextureLoader().load();
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(200));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(400).fill().forEach(addstar);
+
+// const texture2 = new THREE.TextureLoader().load(logo);
+// scene.background=texture2;
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.002;
+  camera.rotation.y = t * -0.002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+  cube2.rotation.x += 0.01;
+  // cube2.rotation.y += 0.01;
+  control.update();
+  renderer.render(scene, camera);
+}
+
+animate();
